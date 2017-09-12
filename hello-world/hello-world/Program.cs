6879a358ffe3fa3262ae944112999350bbbefacc
jannek <janne.kalliokulju@gmail.com>
2017-09-12 10:22:21 +0300

    Created basic hello world example

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            //estää win ikkunan häviämisen näytöltä samantien
+            Console.ReadKey(); 
+        }
+    }
+}
